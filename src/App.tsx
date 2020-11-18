33052c572f03809ea09473c106f2e87e399e2586
shibuya <shibuya@pyxos-jk.co.jp>
2020-11-18 09:44:08 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Hello, React</h1>
+    </div>
+  );
+};
+
+export default App;
