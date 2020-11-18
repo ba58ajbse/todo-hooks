36dc998d0d3267869b9f2dd1bb32bb8fedd6fe32
shibuya <shibuya@pyxos-jk.co.jp>
2020-11-18 11:02:46 +0900

    フォーム関連スタイルの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './assets/App.css';
+import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
@@ -7,6 +8,7 @@ const App: React.FC = () => {
     <div className="App">
       <div className="wrapper">
         <h1>Hello, React</h1>
+        <Form />
         <TodoList />
       </div>
     </div>
