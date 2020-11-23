d1d75aedca5d90fe56519264e72dbfc78ba36ce0
Administrator <admin@example.com>
2020-11-23 11:53:41 +0900

    checkbox追加

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,13 +2,19 @@ import React, { useContext } from 'react';
 import { TodosContext } from '../context/reducer';
 
 type Props = {
-  todo: { id: number; title: string };
+  todo: { id: number; title: string; completed: boolean };
 };
 const Todo: React.FC<Props> = ({ todo }) => {
   const { dispatch } = useContext(TodosContext);
   return (
-    <li key={todo.id}>
-      <p>{todo.title}</p>
+    <li key={todo.id} className={todo.completed ? 'todo-row done' : 'todo-row'}>
+      <input
+        type="checkbox"
+        id={`todo-${todo.id}`}
+        className="checkbox-done"
+        checked={todo.completed}
+      />
+      <label htmlFor={`todo-${todo.id}`}>{todo.title}</label>
       <button
         type="button"
         className="remove-btn"
